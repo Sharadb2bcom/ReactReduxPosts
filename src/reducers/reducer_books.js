e5dd7f33b3ca13fbeb3da5ee29c0095e3ce6aba2
sharadb2bcom <pawar.sharad585@gmail.com>
2018-11-03 21:22:43 +0530

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+	return [
+		{title: "Javascript", pages: 105},
+		{title: "Java", pages: 75},
+		{title: "Html", pages: 65},
+		{title: "CSS", pages: 155},
+		{title: "Jquery", pages: 85}
+
+	];
+}
